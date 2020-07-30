cddd70ce73420c82c268ea10bbe762cf203c3150
santiago <ssantiagotl@gmail.com>
2020-07-30 01:56:24 -0300

    Initial commit

---
+++ src/main/java/com/santiago/springionicstore/SpringionicstoreApplication.java
@@ -0,0 +1,13 @@
+package com.santiago.springionicstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringionicstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringionicstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santiago/springionicstore/SpringionicstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santiago.springionicstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringionicstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
