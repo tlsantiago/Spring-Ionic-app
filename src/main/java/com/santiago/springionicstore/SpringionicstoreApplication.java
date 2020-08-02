94d6839b1a4b1254ea22d352e1aafaf0846496ee
santiago <ssantiagotl@gmail.com>
2020-08-02 18:55:23 -0300

    Adding instantiation operations for DB inside application

--- src/main/java/com/santiago/springionicstore/SpringionicstoreApplication.java
+++ src/main/java/com/santiago/springionicstore/SpringionicstoreApplication.java
@@ -1,13 +1,33 @@
 package com.santiago.springionicstore;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringionicstoreApplication {
+import com.santiago.springionicstore.domain.Categoria;
+import com.santiago.springionicstore.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class SpringionicstoreApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringionicstoreApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
