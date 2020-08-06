b2f57f3e275dc77911611d7c797e56d1eab0793f
santiago <ssantiagotl@gmail.com>
2020-08-06 20:55:48 -0300

    'Estados' and 'Cidades' classes created

--- src/main/java/com/santiago/springionicstore/SpringionicstoreApplication.java
+++ src/main/java/com/santiago/springionicstore/SpringionicstoreApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.santiago.springionicstore.domain.Categoria;
+import com.santiago.springionicstore.domain.Cidade;
+import com.santiago.springionicstore.domain.Estado;
 import com.santiago.springionicstore.domain.Produto;
 import com.santiago.springionicstore.repositories.CategoriaRepository;
+import com.santiago.springionicstore.repositories.CidadeRepository;
+import com.santiago.springionicstore.repositories.EstadoRepository;
 import com.santiago.springionicstore.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,12 @@ public class SpringionicstoreApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringionicstoreApplication.class, args);
@@ -44,6 +54,22 @@ public class SpringionicstoreApplication implements CommandLineRunner {
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		
+
 	}
 
 }
---
+++ src/main/java/com/santiago/springionicstore/repositories/EstadoRepository.java
@@ -0,0 +1,12 @@
+package com.santiago.springionicstore.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.santiago.springionicstore.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+	
+}
