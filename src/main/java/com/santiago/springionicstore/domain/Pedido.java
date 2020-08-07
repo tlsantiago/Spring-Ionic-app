4c86c8e42c682824f48f35ebb33fcad5c9b668bc
santiago <ssantiagotl@gmail.com>
2020-08-07 14:02:22 -0300

    'ItemPedido' class added. All relations needed alreay made

--- src/main/java/com/santiago/springionicstore/domain/Pedido.java
+++ src/main/java/com/santiago/springionicstore/domain/Pedido.java
@@ -2,6 +2,8 @@ package com.santiago.springionicstore.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -32,6 +34,7 @@ public class Pedido implements Serializable{
 	@JoinColumn(name="endereco_de_entrega_id")
 	private Endereco enderecoDeEntrega;
 	
+	private Set<ItemPedido> itens = new HashSet<>();
 	
 	public Pedido() {
 	}
@@ -104,6 +107,14 @@ public class Pedido implements Serializable{
 		return result;
 	}
 
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
 
 	@Override
 	public boolean equals(Object obj) {
@@ -121,6 +132,7 @@ public class Pedido implements Serializable{
 			return false;
 		return true;
 	}
+
 	
 	
 }
--- src/main/java/com/santiago/springionicstore/domain/Produto.java
+++ src/main/java/com/santiago/springionicstore/domain/Produto.java
@@ -2,7 +2,9 @@ package com.santiago.springionicstore.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -32,6 +34,9 @@ public class Produto implements Serializable {
 			)
 	private List<Categoria> categorias = new ArrayList<>();
 	
+	private Set<ItemPedido> itens = new HashSet<>();
+
+	
 	public Produto() {
 	}
 
@@ -42,6 +47,14 @@ public class Produto implements Serializable {
 		this.preco = preco;
 	}
 
+	public List<Pedido> getPedidos() {
+		List<Pedido> lista = new ArrayList<>();
+		for (ItemPedido x : itens) {
+			lista.add(x.getPedido());
+		}
+		return lista;
+	}
+	
 	public Integer getId() {
 		return id;
 	}
@@ -73,6 +86,15 @@ public class Produto implements Serializable {
 	public void setCategorias(List<Categoria> categorias) {
 		this.categorias = categorias;
 	}
+	
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
+	
 
 	@Override
 	public int hashCode() {
@@ -98,7 +120,8 @@ public class Produto implements Serializable {
 			return false;
 		return true;
 	}
-	
+
+
 	
 	
 	
