8d647d4f181df2a67742394b84e19f1c79342fc3
santiago <ssantiagotl@gmail.com>
2020-08-09 23:51:01 -0300

    Endpoint /pedidos/{id} is now available

--- src/main/java/com/santiago/springionicstore/domain/PagamentoComBoleto.java
+++ src/main/java/com/santiago/springionicstore/domain/PagamentoComBoleto.java
@@ -4,13 +4,17 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.santiago.springionicstore.domain.enums.EstadoPagamento;
 
 @Entity
 public class PagamentoComBoleto extends Pagamento {
 	private static final long serialVersionUID = 1L;
 	
+	@JsonFormat(pattern="dd/MM/yyyy")
 	private Date dataVencimento;
+	
+	@JsonFormat(pattern="dd/MM/yyyy")
 	private Date dataPagamento;
 	
 	public PagamentoComBoleto() {
@@ -18,6 +22,7 @@ public class PagamentoComBoleto extends Pagamento {
 
 	public PagamentoComBoleto(Integer id, EstadoPagamento estado, Pedido pedido, Date dataVencimento, Date dataPagamento) {
 		super(id, estado, pedido);
+		
 		this.dataPagamento = dataPagamento;
 		this.dataVencimento = dataVencimento;
 	}
