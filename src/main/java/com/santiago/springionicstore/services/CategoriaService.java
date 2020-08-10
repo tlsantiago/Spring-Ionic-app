e73cfbbbda977a2602d126a5957d2fe662d5a344
santiago <ssantiagotl@gmail.com>
2020-08-10 11:07:39 -0300

    DELETE method created. Exception created to handle Bad Requests

--- src/main/java/com/santiago/springionicstore/services/CategoriaService.java
+++ src/main/java/com/santiago/springionicstore/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.santiago.springionicstore.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.santiago.springionicstore.domain.Categoria;
 import com.santiago.springionicstore.repositories.CategoriaRepository;
+import com.santiago.springionicstore.services.exceptions.DataIntegrityException;
 import com.santiago.springionicstore.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -30,5 +32,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possua produtos associados");
+		}
 	}
 }
