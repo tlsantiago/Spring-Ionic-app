e3bf2b42001fc483ec77f681a55e7df0ab65a657
santiago <ssantiagotl@gmail.com>
2020-08-11 13:56:39 -0300

    Added pagination with optional parameters for data requisition

--- src/main/java/com/santiago/springionicstore/services/CategoriaService.java
+++ src/main/java/com/santiago/springionicstore/services/CategoriaService.java
@@ -1,9 +1,13 @@
 package com.santiago.springionicstore.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.santiago.springionicstore.domain.Categoria;
@@ -42,5 +46,15 @@ public class CategoriaService {
 		catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possua produtos associados");
 		}
+	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),
+				orderBy);		
+		return repo.findAll(pageRequest);
 	}
 }
