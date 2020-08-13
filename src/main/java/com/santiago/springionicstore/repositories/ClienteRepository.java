630e0fa5b4204b43b255ac242e00e90268d0d940
santiago <ssantiagotl@gmail.com>
2020-08-13 15:52:33 -0300

    Validation to prevent duplicated e-mail registration

--- src/main/java/com/santiago/springionicstore/repositories/ClienteRepository.java
+++ src/main/java/com/santiago/springionicstore/repositories/ClienteRepository.java
@@ -1,5 +1,7 @@
 package com.santiago.springionicstore.repositories;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,6 @@ import com.santiago.springionicstore.domain.Cliente;
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 
-	
+	@Transactional
+	Cliente findByEmail(String email);
 }
--- src/main/java/com/santiago/springionicstore/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/santiago/springionicstore/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.santiago.springionicstore.domain.Cliente;
 import com.santiago.springionicstore.domain.enums.TipoCliente;
 import com.santiago.springionicstore.dto.ClienteNewDTO;
+import com.santiago.springionicstore.repositories.ClienteRepository;
 import com.santiago.springionicstore.resources.exception.FieldMessage;
 import com.santiago.springionicstore.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
 	
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -29,6 +36,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email já cadastrado"));
+		}
+		
 		return list.isEmpty();
 	}
 }
