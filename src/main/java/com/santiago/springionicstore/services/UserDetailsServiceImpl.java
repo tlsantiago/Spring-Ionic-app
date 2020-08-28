ef5ca4ea49eb6b03ee474a84a4fc1370cad70141
santiago <ssantiagotl@gmail.com>
2020-08-28 10:02:29 -0300

    Authentication implemented

---
+++ src/main/java/com/santiago/springionicstore/services/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.santiago.springionicstore.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.santiago.springionicstore.domain.Cliente;
+import com.santiago.springionicstore.repositories.ClienteRepository;
+import com.santiago.springionicstore.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = repo.findByEmail(email);
+		if (cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+	}
+}
